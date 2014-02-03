bd3c5fffe3b04ee7f096f7d71d2f4211ae090e4e
tcpiptan <ptan@ptan.info>
2014-02-03 10:01:37 +0900

    modified: setup.sh: makes '~/.dotfiles.old' and then moves old files to it

--- setup.sh
+++ setup.sh
@@ -1,4 +1,26 @@
-FILES=(.bashrc .dir_colors .git-completion .git-prompt .gitconfig .rpmmacros .tmux.conf .vimrc)
+BACKUPDIR=~/.dotfiles.old/
+DIRS=(.vim .vim/bundle .vim/dict)
+FILES=(.bashrc .dir_colors .git-completion .git-prompt .gitconfig .rpmmacros .tmux.conf .vim/dict/php.dict .vimrc)
+
+if [ -e $BACKUPDIR ]; then
+    echo "backup still existing. exit.";
+    exit;
+else
+    mkdir -p $BACKUPDIR
+fi
+
+for i in "${DIRS[@]}"
+do
+    src=~/git/dotfiles/$i
+    dst=~/$i
+
+    if [ -e $dst ]; then
+        mv $dst $BACKUPDIR
+    fi
+    if [ ! -e $dst ]; then
+        mkdir -p $dst
+    fi
+done
 
 for i in "${FILES[@]}"
 do
@@ -6,14 +28,9 @@ do
     dst=~/$i
 
     if [ -e $dst ]; then
-        rm -rf $dst
+        mv $dst $BACKUPDIR
     fi
     ln -s $src $dst
 done
-
-rm -rf ~/.vim/bundle
-mkdir -p ~/.vim/{dict,bundle}
-
-ln -s ~/git/dotfiles/.vim/dict/php.dict ~/.vim/dict/php.dict
 
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle
